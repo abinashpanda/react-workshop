7e105afe23ae6ecf6109a83a263cba60aa6bc3a8
Abinash Panda <abinash@prodios.com>
2022-12-31 14:22:03 +0530

    feat(auth): remove auth

--- src/app.tsx
+++ src/app.tsx
@@ -1,37 +1,10 @@
-import { Spin } from 'antd'
-import AppShell from 'components/app-shell'
-import AuthProtection from 'components/auth-protection'
-import { useAuthContext } from 'hooks/use-auth'
 import CompoundComponent from 'pages/compound-component'
-import Dashboard from 'pages/dashboard'
 import Login from 'pages/login'
-import { Outlet, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
-  const { authVerificationInProgress } = useAuthContext()
-
-  if (authVerificationInProgress) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <Spin tip="Authenticating User" />
-      </div>
-    )
-  }
-
   return (
     <Routes>
-      <Route
-        element={
-          <AuthProtection>
-            <AppShell>
-              <Outlet />
-            </AppShell>
-          </AuthProtection>
-        }
-      >
-        {/* TODO: Implement lazy loading */}
-        <Route path="" element={<Dashboard />} />
-      </Route>
       <Route path="compound-component" element={<CompoundComponent />} />
       <Route path="login" element={<Login />} />
     </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,13 @@ import { ANTD_THEME } from 'styles/theme'
 import { QueryClientProvider } from 'react-query'
 import { queryClient } from 'utils/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from 'hooks/use-auth'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <ConfigProvider theme={ANTD_THEME}>
         <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
+          <App />
         </QueryClientProvider>
       </ConfigProvider>
     </BrowserRouter>
